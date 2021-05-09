163b06eb14de9d948da57a805c4c1e84ff60f336
andreswebs <andreswebs@pm.me>
2021-05-09 15:03:03 -0300

    update Dockerfile and src

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,19 @@
-FROM node:lts AS base
+FROM node:16-buster-slim AS base
 
 RUN \
+  apt-get update > /dev/null && \
+  apt-get install -qy perl-modules git && \
   deluser --remove-home node && \
-  groupadd --gid 1000 nodered && \
-  useradd --gid nodered --uid 1000 --shell /bin/bash --create-home nodered
+  groupadd --gid 1000 node-red && \
+  useradd --gid node-red --uid 1000 --shell /bin/bash --create-home node-red && \
+  apt-get remove -qy perl-modules && \
+  apt-get clean && \
+  apt-get autoremove -y && \
+  rm -rf /var/lib/apt/lists/*
 
 RUN \
   mkdir -p /data && \
-  chown 1000:1000 /data
+  chown node-red:node-red /data
 
 WORKDIR /data
 
@@ -15,25 +21,28 @@ WORKDIR /data
 FROM base AS build
 
 RUN \
-  apt-get update && \
-  apt-get install -qy build-essential python perl-modules
+  apt-get update > /dev/null && \
+  apt-get install -qy \
+    gcc \
+    g++ \
+    make \
+    perl-modules \
+    python3 && \
+  ln -s /usr/bin/python3 /usr/bin/python && \
+  rm -rf /var/lib/apt/lists/*
 
-COPY --chown=1000:1000 ./package.json /data/
+COPY --chown=node-red:node-red ./src/* /data/
 
-USER 1000
+USER node-red
 
-RUN npm install --production
+RUN \
+  npm install --production
 
 ## Release
 FROM base AS release
 
-RUN \
-  apt-get update && \
-  apt-get install -y perl-modules && \
-  rm -rf /var/lib/apt/lists/*
-
-COPY --chown=1000:1000 ./* /data/
-COPY --from=build --chown=1000:1000 /data/node_modules /data/node_modules
+COPY --chown=node-red:node-red ./src/* /data/
+COPY --from=build --chown=node-red:node-red /data/node_modules /data/node_modules
 
 ENV PORT=1880
 ENV NODE_ENV=production
@@ -41,6 +50,6 @@ ENV NODE_PATH=/data/node_modules
 ENV NODE_RED_HOME=/data
 EXPOSE 1880
 
-USER 1000
+USER node-red
 
 CMD ["node", "/data/server.js", "/data/flows.json"]
---
+++ src/server.js
@@ -0,0 +1,107 @@
+const http = require('http');
+const https = require('https');
+const express = require('express');
+const fs = require('fs');
+const RED = require('node-red');
+const health = require('@cloudnative/health-connect');
+
+// Create an Express app
+let server;
+const app = express();
+
+process.env.NODE_RED_HOME = process.env.NODE_RED_HOME || __dirname;
+
+// Add a simple route for static content served from 'public'
+app.use('/', express.static('public'));
+
+const healthcheck = new health.HealthChecker();
+app.use('/live', health.LivenessEndpoint(healthcheck));
+app.use('/ready', health.ReadinessEndpoint(healthcheck));
+app.use('/health', health.HealthEndpoint(healthcheck));
+
+// Create the settings object from file
+const settings = require('./settings.js');
+
+if (process.env.NODE_RED_TLS_CERT && process.env.NODE_RED_TLS_KEY) {
+  settings.https = {
+    key: fs.readFileSync(process.env.NODE_RED_TLS_KEY),
+    cert: fs.readFileSync(process.env.NODE_RED_TLS_CERT),
+  };
+  if (process.env.NODE_RED_TLS_CA) {
+    settings.https.ca = fs.readFileSync(process.env.NODE_RED_TLS_CA);
+  }
+  if (process.env.NODE_RED_TLS_PASS_FILE) {
+    settings.https.passphrase = fs.readFileSync(
+      process.env.NODE_RED_TLS_PASS_FILE
+    );
+  } else if (process.env.NODE_RED_TLS_PASS_ENV) {
+    settings.https.passphrase = process.env.NODE_RED_TLS_PASS_ENV;
+  }
+  settings.requireHttps = true;
+}
+
+if (settings.https) {
+  server = https.createServer(settings.https, (req, res) => {
+    app(req, res);
+  });
+} else {
+  server = http.createServer((req, res) => {
+    app(req, res);
+  });
+}
+
+server.setMaxListeners(0);
+
+function formatRoot(root) {
+  if (root[0] !== '/') {
+    root = `/${root}`;
+  }
+  if (root.slice(-1) !== '/') {
+    root += '/';
+  }
+  return root;
+}
+
+if (!settings.httpRoot) {
+  settings.httpAdminRoot = false;
+  settings.httpNodeRoot = false;
+} else {
+  settings.httpRoot = settings.httpRoot || '/';
+  settings.disableEditor = settings.disableEditor || false;
+}
+
+if (settings.httpAdminRoot) {
+  settings.httpAdminRoot = formatRoot(
+    settings.httpAdminRoot || settings.httpRoot || '/'
+  );
+  settings.httpAdminAuth = settings.httpAdminAuth || settings.httpAuth;
+} else {
+  settings.disableEditor = true;
+}
+
+if (settings.httpNodeRoot) {
+  settings.httpNodeRoot = formatRoot(
+    settings.httpNodeRoot || settings.httpRoot || '/'
+  );
+  settings.httpNodeAuth = settings.httpNodeAuth || settings.httpAuth;
+}
+
+if (!settings.uiPort) {
+  settings.uiPort = 1880;
+}
+
+settings.uiHost = settings.uiHost || '0.0.0.0';
+
+// Initialise the runtime with a server and settings
+RED.init(server, settings);
+
+// Serve the editor UI from /red
+app.use(settings.httpAdminRoot, RED.httpAdmin);
+
+// Serve the http nodes UI from /api
+app.use(settings.httpNodeRoot, RED.httpNode);
+
+server.listen(settings.uiPort);
+
+// Start the runtime
+RED.start();
